2ef9cda8495c1182164d162994a4b51058f39d6e
Ravish Bisht <ravishbisht85@gmail.com>
2024-06-22 17:40:49 +0530

    add styles in form

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,14 +17,14 @@ export const Home = () => {
         <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
           Polish your javaScript knowledge by playing quizzes.
         </p>
-        <div className="flex justify-center my-10">
+        <button className="flex justify-center my-10">
           <NavLink
             to="/playquiz"
             className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md"
           >
             Play The Quiz
           </NavLink>
-        </div>
+        </button>
         <div className="flex md:flex-row flex-col mt-10 justify-center items-center">
           <img
             className="rounded-lg w-[22rem] h-[15rem] border border-orange-700 shadow-sm shadow-orange-400 mx-2 my-4"
--- frontend/src/pages/QuizForm.jsx
+++ frontend/src/pages/QuizForm.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 
 export const QuizForm = () => {
-  //storing all the questions data in a single object with useState
   const [questionData, setQuestionData] = useState({
     question: "",
     option1: "",
@@ -13,7 +12,6 @@ export const QuizForm = () => {
     answer: "",
   });
 
-  //This function will run everyTime we insert some characters and update it in questionData object.
   function handleChange(e) {
     setQuestionData({
       ...questionData,
@@ -21,19 +19,17 @@ export const QuizForm = () => {
     });
   }
 
-  //This function will run when we will send questions data to backend
   const handleSubmit = async (e) => {
-    e.preventDefault();  //prevent the default behaviour of form so that the form will not refresh automatically
+    e.preventDefault();
 
     const { question, option1, option2, option3, option4, answer } =
-      questionData;  //extracting the fields from questionData by destructuring it.
+      questionData;
 
-    //setting all the fields in a way that we have set in backend so that the backend api will accept this data.
-    const quizData = { 
+    const quizData = {
       question: question,
       options: [option1, option2, option3, option4],
       answer,
-    }; 
+    };
 
     try {
       const response = await fetch("http://localhost:5000/api/quiz/add-quiz", {
@@ -45,7 +41,6 @@ export const QuizForm = () => {
       });
       const result = await response.json();
 
-    //using react-hot-toast for toast message
       toast.success(result.msg, {
         style: {
           minWidth: "300px",
@@ -54,7 +49,6 @@ export const QuizForm = () => {
         },
       });
 
-      // Reset all the  fields
       setQuestionData({
         question: "",
         option1: "",
@@ -65,17 +59,20 @@ export const QuizForm = () => {
       });
       console.log(result);
     } catch (error) {
-      toast.error(error || "something went wrong in adding toast");
-      console.log(error || "something went wrong in post request");
+      toast.error(error.message || "Something went wrong in adding toast");
+      console.log(error.message || "Something went wrong in post request");
     }
   };
 
   return (
-    <section>
-      <form onSubmit={handleSubmit}>
+    <section className="p-6 max-w-lg mx-auto mt-10 mb-10">
+      <form className="space-y-4 border border-orange-700 shadow-orange-400 p-6 rounded-lg shadow-lg" onSubmit={handleSubmit}>
         <div>
-          <label>Question </label>
+          <label className="block text-orange-600 font-bold mb-2">
+            Question
+          </label>
           <input
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-100"
             type="text"
             name="question"
             value={questionData.question}
@@ -83,8 +80,11 @@ export const QuizForm = () => {
           />
         </div>
         <div>
-          <label>Option 1 </label>
+          <label className="block text-orange-600 font-bold mb-2">
+            Option 1
+          </label>
           <input
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-100"
             type="text"
             name="option1"
             value={questionData.option1}
@@ -92,8 +92,11 @@ export const QuizForm = () => {
           />
         </div>
         <div>
-          <label>Option 2 </label>
+          <label className="block text-orange-600 font-bold mb-2">
+            Option 2
+          </label>
           <input
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-100"
             type="text"
             name="option2"
             value={questionData.option2}
@@ -101,8 +104,11 @@ export const QuizForm = () => {
           />
         </div>
         <div>
-          <label>Option 3 </label>
+          <label className="block text-orange-600 font-bold mb-2">
+            Option 3
+          </label>
           <input
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-100"
             type="text"
             name="option3"
             value={questionData.option3}
@@ -110,8 +116,11 @@ export const QuizForm = () => {
           />
         </div>
         <div>
-          <label>Option 4 </label>
+          <label className="block text-orange-600 font-bold mb-2">
+            Option 4
+          </label>
           <input
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-100"
             type="text"
             name="option4"
             value={questionData.option4}
@@ -119,15 +128,21 @@ export const QuizForm = () => {
           />
         </div>
         <div>
-          <label>Answer</label>
+          <label className="block text-orange-600 font-bold mb-2">Answer</label>
           <input
+            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-100"
             type="text"
             name="answer"
             value={questionData.answer}
             onChange={handleChange}
           />
         </div>
-        <button type="submit">Add Question</button>
+        <button
+          className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 rounded-md w-full hover:bg-orange-800"
+          type="submit"
+        >
+          Add Question
+        </button>
       </form>
     </section>
   );
