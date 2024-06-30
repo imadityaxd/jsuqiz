8b6440dca211755d68b7cf29ab0e8df69aec81c6
Ravish Bisht <ravishbisht85@gmail.com>
2024-06-30 11:19:02 +0530

    result page add

--- frontend/src/components/Result.jsx
+++ frontend/src/components/Result.jsx
@@ -1,4 +1,29 @@
-export default function Result({ questions }) {
-  console.log("resul", questions);
-  return <div>Result</div>;
+import { Link } from "react-router-dom";
+
+export default function Result(prop) {
+  console.log("resul", prop.questions);
+  return (
+    <div className="px-14 py-12">
+      <div className="">
+        {prop.questions.map((question, index) => (
+          <>
+            <h2 className="text-2xl text-orange-500 py-1">{`Q${index + 1}. ${
+              question.question
+            }`}</h2>
+            <h3 className="text-white text-xl pb-6">
+              <span className="text-orange-500">Ans.</span>
+              {` ${question.answer}`}
+            </h3>
+          </>
+        ))}
+      </div>
+      <div className="flex w-full gap-4">
+        <Link to="/">
+          <button className="bg-orange-500 px-2 py-2 rounded-lg">
+            Go to home page
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
