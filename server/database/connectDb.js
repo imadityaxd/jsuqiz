38386ebe6146a60abd6cc350a177bb9a32074746
Aditya Sharma <104629524+imadityaxd@users.noreply.github.com>
2025-07-26 09:28:58 +0530

    fixing the database

--- server/database/connectDb.js
+++ server/database/connectDb.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
+
 export async function dbConnect() {
   try {
-    await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`); //connecting database
+    await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true, // enable SSL/TLS
+      tlsAllowInvalidCertificates: true, // allows self-signed certs (only for local dev!)
+    });
     console.log("db connected successfully");
   } catch (error) {
     console.log("db failed to connect. Error: ", error);
-    process.exit(1);  
+    process.exit(1);
   }
 }
