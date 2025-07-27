7bd2c70e2a5e37d0c39f9844d9dcdc298ec9f7cc
Aditya Sharma <104629524+imadityaxd@users.noreply.github.com>
2025-07-27 10:44:10 +0530

    setting the links

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,8 @@
 import AuthForm from "../components/AuthForm";
-
+import authbg from "../assets/authbg.png"
 export default function Login() {
   return (
-    <div className="text-white h-[100vh] flex items-center justify-center bg-cover" style={{backgroundImage:"url('../src/assets/authbg.png')"}}>
+    <div className="text-white h-[100vh] flex items-center justify-center bg-cover" style={{backgroundImage:"url('../assets/authbg.png')"}}>
     <AuthForm formName="Login" goTo="/signup" navigate="Signup" api="login" />
   </div>
   )
