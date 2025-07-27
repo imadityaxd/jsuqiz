0cd0241279e91fb4b554c2e6f09aeb4272ff4194
Aditya Sharma <104629524+imadityaxd@users.noreply.github.com>
2025-07-27 10:50:52 +0530

    image fix

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import AuthForm from "../components/AuthForm";
 import authbg from "../assets/authbg.png"
 export default function Login() {
   return (
-    <div className="text-white h-[100vh] flex items-center justify-center bg-cover" style={{backgroundImage:"url('../assets/authbg.png')"}}>
+    <div className="text-white h-[100vh] flex items-center justify-center bg-cover" style={{backgroundImage:`url(${authbg})`}}>
     <AuthForm formName="Login" goTo="/signup" navigate="Signup" api="login" />
   </div>
   )
